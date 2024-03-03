5d75796ba62f328c4a0572dde65383adbcbb029d
Drew Herron <herron@mailbox.org>
2024-03-03 01:49:30 -0800

    Import SentenceGenerator

--- app.py
+++ app.py
@@ -2,11 +2,15 @@
 A simple guestbook flask app.
 """
 import flask, os
+from sentence_generator import SentenceGenerator
 from flask.views import MethodView
 from index import Index
 from sign import Sign
 
-app = flask.Flask(__name__)       # our Flask app
+app = flask.Flask(__name__)
+generator = SentenceGenerator(openai_api_key=os.getenv('OPENAI_API_KEY'))
+model = get_model()
+
 
 app.add_url_rule('/',
                  view_func=Index.as_view('index'),
