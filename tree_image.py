435f99d44c389ed4f2286111a9f50a3ae8d6cebb
Drew Herron <herron@mailbox.org>
2024-03-07 15:29:17 -0800

    Switch to custom svgling package

--- tree_image.py
+++ tree_image.py
@@ -1,16 +1,19 @@
 # tree_image.py
 import sys
-#sys.path.insert(0, '/Projects/svgling')
+sys.path.insert(0, '/home/drew/Projects/svgling')
 import svgling
-#sys.path.remove('/Projects/svgling')
+sys.path.remove('/home/drew/Projects/svgling')
+print(svgling.__file__)
 from nltk.tree import Tree
+
 
 def generate_syntax_tree_image(tree_structure, tree_movement, output_filepath='static/images/last_tree.svg'):
     # Parse the tree structure
     tree = Tree.fromstring(tree_structure)
 
     # Draw the tree with svgling
-    tree_figure = svgling.draw_tree(tree)
+    tree_figure = svgling.draw_tree(tree, leaf_edges=False)
+    #tree_figure = svgling.draw_tree(tree)
 
     # Apply movement arrows if provided
     if tree_movement:
@@ -29,7 +32,7 @@ def generate_syntax_tree_image(tree_structure, tree_movement, output_filepath='s
 # For testing
 if __name__ == '__main__':
     filepath = "test_tree.svg"
-    test_tree_structure = "(CP (C' (C \u2205) (TP (DP (D' (D \u2205) (NP (N' (N She))))) (T' (T [-PAST]) (ProgP (Prog' (Prog is)  (VP (V' (AdvP (Adv' (Adv probably))) (V' (V' (V having) (DP (D' (D a) (NP (N' (N snack)))))) (PP (P' (P without) (DP (D' (D \u2205) (NP (N' (N me)))))))) ) )))))))"
+    test_tree_structure = "(CP (C' (C\n\u2205) (TP (DP (D' (D \u2205) (NP (N' (N She))))) (T' (T [-PAST]) (ProgP (Prog' (Prog is)  (VP (V' (AdvP (Adv' (Adv probably))) (V' (V' (V having) (DP (D' (D a) (NP (N' (N snack)))))) (PP (P' (P without) (DP (D' (D \u2205) (NP (N' (N me))))))))))))))))"
 
     generate_syntax_tree_image(test_tree_structure, None, filepath)
     print(f"Saved the SVG of the provided tree structure to {filepath}.")
