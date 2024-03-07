c416910ba361965b9b18dde2a3c541260609bdfc
Drew Herron <herron@mailbox.org>
2024-03-06 23:42:43 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,27 @@
-"""
-A simple guestbook flask app.
-"""
 import flask, os
-from sentence_generator import SentenceGenerator
 from flask.views import MethodView
-from index import Index
-from sign import Sign
+from sentence_generator import SentenceGenerator
+from tree_image import generate_syntax_tree_image
+#from tree_model import get_model
+import uuid
 
 app = flask.Flask(__name__)
 generator = SentenceGenerator(openai_api_key=os.getenv('OPENAI_API_KEY'))
-model = get_model()
+#model = get_model()
 
+@app.route('/')
+def index():
 
-app.add_url_rule('/',
-                 view_func=Index.as_view('index'),
-                 methods=["GET"])
+    sentence, tree_structure, tree_movement = generator.generate_filled_sentence_and_tree()
 
-app.add_url_rule('/sign/',
-                 view_func=Sign.as_view('sign'),
-                 methods=['GET', 'POST'])
+    tree_image_path = f"static/images/syntax_tree_{uuid.uuid4()}.svg"
+    generate_syntax_tree_image(tree_structure, tree_movement, tree_image_path)
+
+    # ?
+    #model.insert(sentence, tree_image_path) #gif_path?
+
+    # Render the index page with the sentence and path to the syntax tree image
+    return render_template('index.html', sentence=sentence, tree_image_path=tree_image_path)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=int(os.environ.get('PORT',5000)))
+    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT',5000)))
