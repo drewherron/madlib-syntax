ae70329e7174afb6fe0bb23654c2504bc1dcc91a
Drew Herron <herron@mailbox.org>
2024-03-10 12:14:58 -0700

    Add function to fetch related gif

---
+++ related_gif.py
@@ -0,0 +1,37 @@
+import requests
+import os
+
+def fetch_gif_url(query):
+    api_key = os.getenv('GIPHY_API_KEY')
+    base_url = 'https://api.giphy.com/v1/gifs/search'
+    params = {
+        'api_key': api_key,
+        'q': query,
+        'limit': 1,
+        'rating': 'pg'
+    }
+    response = requests.get(base_url, params=params)
+    print(f"\nRESPONSE:\n{response}")
+    if response.status_code == 200:
+#        data = response.json()
+#        if data['data']:
+#            gif_url = data['data'][0]['images']['original']['url']
+#            return gif_url
+#    return None
+
+        data = response.json()
+        print(f"\nDATA:\n{data}")
+        if data['data']:
+            downsized = data['data'][0]['images']['downsized']['url']
+            return downsized
+    return None
+
+
+if __name__ == "__main__":
+
+    test_query = "funny cat"
+    gif_url = fetch_gif_url(test_query)
+    if gif_url:
+        print("GIF URL:", gif_url)
+    else:
+        print("No GIF found for the query.")
